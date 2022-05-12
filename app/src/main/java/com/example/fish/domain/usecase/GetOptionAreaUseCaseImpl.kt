8e1a185078f673dbbc37bb00a72ba91eb9992b96
utamahendra <utama.hendra19@gmail.com>
2022-05-12 23:19:27 +0700

    create get option area use case and change return value get option size use case

--- app/src/main/java/com/example/fish/domain/usecase/GetOptionAreaUseCaseImpl.kt
+++ app/src/main/java/com/example/fish/domain/usecase/GetOptionAreaUseCaseImpl.kt
@@ -2,15 +2,15 @@ package com.example.fish.domain.usecase
 
 import com.example.fish.common.extension.asErrorObject
 import com.example.fish.common.viewstate.ViewError
-import com.example.fish.data.remote.response.OptionSizeResponse
+import com.example.fish.data.remote.response.OptionAreaResponse
 import com.example.fish.domain.Either
 import com.example.fish.domain.source.FishDataSource
 
-class GetOptionSizeUseCaseImpl(private val repository: FishDataSource) : GetOptionSizeUseCase {
+class GetOptionAreaUseCaseImpl(private val repository: FishDataSource) : GetOptionAreaUseCase {
 
-    override suspend fun invoke(params: Unit): Either<OptionSizeResponse, ViewError> {
+    override suspend fun invoke(params: Unit): Either<OptionAreaResponse, ViewError> {
         return try {
-            val result = repository.getOptionSize()
+            val result = repository.getOptionArea()
             Either.Success(result)
         } catch (e: Exception) {
             Either.Fail(e.asErrorObject())
